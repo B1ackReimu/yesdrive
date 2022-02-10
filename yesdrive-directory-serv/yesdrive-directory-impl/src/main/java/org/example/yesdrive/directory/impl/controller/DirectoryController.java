37e8b075c1c0bec972d3e688830195354630200e
BlackReimu <blackreimu@toho.xyz>
2022-02-10 22:40:26 +0800

    feat(directory impl):

---
+++ yesdrive-directory-serv/yesdrive-directory-impl/src/main/java/org/example/yesdrive/directory/impl/controller/DirectoryController.java
@@ -0,0 +1,12 @@
+package org.example.yesdrive.directory.impl.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/directory")
+public class DirectoryController {
+
+
+
+}
---
+++ yesdrive-directory-serv/yesdrive-directory-impl/src/main/java/org/example/yesdrive/directory/impl/service/DirectoryService.java
@@ -0,0 +1,13 @@
+package org.example.yesdrive.directory.impl.service;
+
+import org.example.yesdrive.directory.api.beans.DirectoryInfo;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DirectoryService {
+
+    public void createDirectory(DirectoryInfo directoryInfo){
+
+    }
+
+}
