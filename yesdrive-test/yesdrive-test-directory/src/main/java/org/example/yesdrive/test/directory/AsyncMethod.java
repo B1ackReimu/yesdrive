6c8f03c6b0bebcf8e6070092712a45d4659b218a
BlackReimu <blackreimu@toho.xyz>
2022-02-14 07:03:57 +0800

    feat(directory impl,common,test): new test template
    new random test template

--- yesdrive-test/yesdrive-test-directory/src/main/java/org/example/yesdrive/test/directory/AsyncMethod.java
+++ yesdrive-test/yesdrive-test-directory/src/main/java/org/example/yesdrive/test/directory/AsyncMethod.java
@@ -3,18 +3,38 @@ package org.example.yesdrive.test.directory;
 
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class AsyncMethod {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException, ExecutionException {
         ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
         executor.setCorePoolSize(20);
         executor.setMaxPoolSize(40);
         executor.initialize();
 
-        Runnable runnable = () -> System.out.println(Thread.currentThread().getName());
+        AtomicInteger atomicInteger1 = new AtomicInteger();
+        Runnable runnable = () -> {
+            try {
+                System.out.println(Thread.currentThread().getName());
+                TimeUnit.SECONDS.sleep(5);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                atomicInteger1.incrementAndGet();
+            }
+        };
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i < 20; i++) {
             executor.execute(runnable);
+        }
+        while (atomicInteger1.get() < 20) {
+            TimeUnit.NANOSECONDS.sleep(1);
         }
         executor.shutdown();
     }
